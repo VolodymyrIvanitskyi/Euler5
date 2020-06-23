b63adb2fbeb4366e8dad61c480752686c59fe2cb
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 17:14:20 +0300

    Euler5 is completed

--- Euler5/Program.cs
+++ Euler5/Program.cs
@@ -6,7 +6,39 @@ namespace Euler5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 20;
+            while(true)
+            {
+                n++;
+                if (n % 2 == 0 &&
+                    n % 3 == 0 && 
+                    n % 4 == 0 && 
+                    n % 5 == 0 &&
+                    n % 6 == 0 && 
+                    n % 7 == 0 &&
+                    n % 8 == 0 &&
+                    n % 9 == 0 &&
+                    n % 10 == 0 &&
+                    n % 11 == 0 &&
+                    n % 12 == 0 &&
+                    n % 13 == 0 &&
+                    n % 14 == 0 &&
+                    n % 15 == 0 &&
+                    n % 16 == 0 &&
+                    n % 17 == 0 &&
+                    n % 18 == 0 &&
+                    n % 19 == 0 &&
+                    n % 20 == 0 )
+                {
+                    Console.WriteLine($"Figure {n}");
+                    return;
+                }
+            }
         }
+
+    
+
+
     }
+
 }
